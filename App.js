ae6707806134dd439d9d440cb7d4e5c3b92cd86f
Mert Guner <mertcannguner@gmail.com>
2021-12-14 00:46:26 +0300

    react-native

---
+++ App.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import { Actions, Router, Scene } from 'react-native-router-flux';
+import Inputs from './Inputs';
+
+
+const Home = () => {
+  const goToAbout = () => {
+    Actions.about();
+  };
+
+  return(
+<View>
+      <TouchableOpacity style={{margin: 128}} onPress={goToAbout}>
+      <Text>This is HOME!</Text>
+      
+      </TouchableOpacity>
+      <Inputs/>
+      </View>
+  );
+};
+
+const About = () => {
+  
+  const goToHome = () => {
+    Actions.home();
+  };
+  
+  return(
+    <View style={{ backgroundColor: '#1FA2DA'}}> 
+    <TouchableOpacity style={{margin: 128}} onPress={goToHome}>
+      <Text>This is ABOUT!</Text>
+    </TouchableOpacity>
+   </View>
+  );
+};
+
+const App = () => {
+  return(
+  <Router>
+    <Scene>
+      <Scene key="home" component={Home} title="Home" initial={true}></Scene>
+      <Scene key="about" component={About} title="About"></Scene>
+    </Scene>
+  </Router>
+  )
+};
+
+
+export default App;
